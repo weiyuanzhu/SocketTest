666a93def29d9fc08971b725b0c002bce08e6084
weiyuanzhu <weiyuan.zhu@mackwell.com>
2014-01-17 14:29:51 +0000

    test commit

---
+++ src/mackwell/nlight/LoadingScreen.java
@@ -0,0 +1,49 @@
+package mackwell.nlight;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+
+import com.example.nclient.R;
+
+public class LoadingScreen extends Activity {
+	
+	private static int delay = 3000;
+	private Handler loadHandler = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_loading_screen);
+		
+		loadHandler = new Handler();
+		loadHandler.postDelayed(load, delay);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.loading_screen, menu);
+		return true;
+	}
+	
+	Runnable load = new Runnable()
+	{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+			Intent i = new Intent(LoadingScreen.this, PanelListActivity.class);
+			startActivity(i);
+			
+			finish();
+			
+		}
+		
+		
+	};
+
+}
