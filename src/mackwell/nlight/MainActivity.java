cd9540066dfbe8b2ade74d899b2379ab72471dbb
weiyuanzhu <weiyuan.zhu@mackwell.com>
2014-02-12 09:49:39 +0000

    Update: PanelInfo background picture

--- src/mackwell/nlight/MainActivity.java
+++ src/mackwell/nlight/MainActivity.java
@@ -5,15 +5,21 @@ import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.nclient.R;
 
 public class MainActivity extends Activity implements OnListItemClickedCallBack{
+	
+	private ImageView panelInfoImage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		panelInfoImage = (ImageView) findViewById(R.id.panelInfo_image);
+		
 	}
 
 	@Override
@@ -28,6 +34,7 @@ public class MainActivity extends Activity implements OnListItemClickedCallBack{
 	@Override
 	public void onListItemClicked(String ip, String location) {
 		
+		panelInfoImage.setVisibility(View.INVISIBLE);
 		System.out.println(location + " " +  ip);
 		
 		PanelInfoFragment panelFragment = PanelInfoFragment.newInstance(ip, location);
