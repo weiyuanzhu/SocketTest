a3e9cbe4dbd477e481569905327ad418ecd2bccf
weiyuanzhu <weiyuan.zhu@mackwell.com>
2014-02-24 10:38:31 +0000

    rename MainActivity to PanelActivity

--- src/mackwell/nlight/PanelActivity.java
+++ src/mackwell/nlight/PanelActivity.java
@@ -14,7 +14,7 @@ import android.widget.ImageView;
 
 import com.example.nclient.R;
 
-public class MainActivity extends Activity implements OnListItemClickedCallBack{
+public class PanelActivity extends Activity implements OnListItemClickedCallBack{
 	
 	private List<Panel> panelList = null;
 	
