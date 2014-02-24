d7299adfd56f9f40e703325b15672da67bf67262
weiyuanzhu <weiyuan.zhu@mackwell.com>
2014-02-24 16:38:41 +0000

    rename activity_main.xml to activity_panel.xml

--- src/mackwell/nlight/PanelActivity.java
+++ src/mackwell/nlight/PanelActivity.java
@@ -35,7 +35,7 @@ public class PanelActivity extends Activity implements OnListItemClickedCallBack
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_panel);
 		panelInfoImage = (ImageView) findViewById(R.id.panelInfo_image);
 	
 		fragmentList = new ArrayList<PanelInfoFragment>(5);
