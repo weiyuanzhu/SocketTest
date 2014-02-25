ad713e4976038d0410e3b5d1f38b0fcce22a5444
weiyuanzhu <weiyuan.zhu@mackwell.com>
2014-02-25 09:50:20 +0000

    added: DeviceActivity,DeviceListFragment, DeviceInfoFragment

---
+++ src/mackwell/nlight/DeviceActivity.java
@@ -0,0 +1,26 @@
+package mackwell.nlight;
+
+import com.example.nclient.R;
+import com.example.nclient.R.layout;
+import com.example.nclient.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class DeviceActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_device);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.device, menu);
+		return true;
+	}
+
+}
