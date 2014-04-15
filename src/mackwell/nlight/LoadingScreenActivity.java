0b19b164360ae7989128b4ca7686e1fdb1bd7b71
weiyuanzhu <weiyuan.zhu@mackwell.com>
2014-04-15 11:47:23 +0100

    conflict test, made on feature branch

--- src/mackwell/nlight/LoadingScreenActivity.java
+++ src/mackwell/nlight/LoadingScreenActivity.java
@@ -32,6 +32,7 @@ import com.example.nclient.R;
 /**
  * @author weiyuan zhu
  * 15/04/2014 Starting develop branch
+ * test 2 on feature branch
  */
 public class LoadingScreenActivity extends BaseActivity{
 	
